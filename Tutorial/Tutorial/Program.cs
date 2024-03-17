3047bbbded3d79b15ddab53591f8d16fad3ba70a
s28210 <s28210@pjwstk.edu.pl>
2024-03-18 00:13:06 +0100

    Calculate the average of an array of integers

--- Tutorial/Tutorial/Program.cs
+++ Tutorial/Tutorial/Program.cs
@@ -1,6 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿namespace Tutorial;
 
-Console.WriteLine("This is modification 3!");
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Example usage:
+        int[] numbers = { 1, 2, 3, 4, 5 };
+        double average = CalculateAverage(numbers);
+        Console.WriteLine("Average: " + average);
+    }
+
+    public static double CalculateAverage(int[] numbers)
+    {
+        if (numbers == null || numbers.Length == 0)
+        {
+            throw new ArgumentException("Array cannot be null or empty");
+        }
+
+        int sum = 0;
+        foreach (int num in numbers)
+        {
+            sum += num;
+        }
+
+        // Calculate average
+        double average = (double)sum / numbers.Length;
+        return average;
+    }
+}
 
 
 
